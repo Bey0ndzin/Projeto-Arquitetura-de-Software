cc796e6ca132ed1e7828a523532415d48b86a9a7
Pedro Nunes <zyxblackbeard@gmail.com>
2023-06-21 10:44:16 -0300

    api pt2

--- API/index.js
+++ API/index.js
@@ -11,7 +11,51 @@ const devedores = require("./devedores.js");
 
 connection.connect();
 
-app.get('/', async (req, res) =>
+function middleWareGlobal (req, res, next)
+{
+    console.time('Duração'); // marca o início da requisição
+    console.log('Iniciou  o processamento da requisição '+req.method+' em '+req.url); // indica o início do processamento da url, indicando também o método
+
+    next(); // função que chama o processamento, propriamente dito, da requisição
+               
+    console.log('Terminou o processamento da requisição '+req.method+' em '+req.url); // indica o término do processamento da url, indicando também o método
+    console.timeEnd('Duração'); // informa duração do processamento da requisição
+}
+
+async function ativacaoDoServidor ()
+{
+    const ret = await bd.estrutureSe();
+
+    if (ret===null)
+    {
+        console.log ('Não foi possível estabelecer conexão com o BD!');
+        process.exit(1);
+    }
+
+    if (ret===false)
+    {
+        console.log ('Não foi possível estruturar o BD!');
+        process.exit(1);
+    }
+
+    const express = require('express');
+    const app     = express();
+    
+    app.use(express.json());   // faz com que o express consiga processar JSON
+    app.use(middleWareGlobal); // app.use cria o middleware global
+
+    app.post  ('/livros'        , rotas.inclusao); 
+    app.put   ('/livros/:codigo', rotas.atualizacao);
+    app.delete('/livros/:codigo', rotas.remocao);
+    app.get   ('/livros/:codigo', rotas.recuperacaoDeUm);
+    app.get   ('/livros'        , rotas.recuperacaoDeTodos);
+
+    console.log ('Servidor ativo na porta 3000...');
+    app.listen(3000);
+}
+ativacaoDoServidor();
+
+/*app.get('/', async (req, res) =>
 {
   var id = parseInt(req.query.id);
   var retorno;
@@ -23,11 +67,11 @@ app.get('/', async (req, res) =>
   }
     
   res.send(retorno)
-    /*connection.query(sql, function (err, rows, fields) {
+    connection.query(sql, function (err, rows, fields) {
       if (err) throw err
   
       res.send(rows)
-    })*/
+    })
 })
 
 app.get('/incluir', async (req, res) =>
@@ -80,4 +124,4 @@ app.get('/deletar', async (req, res) =>
   
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
-})
+})*/
